418dbed4f61f1c40150a49e4f018ed350d900350
shamim19089 <144156936+shamim19089@users.noreply.github.com>
2023-09-06 20:52:44 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+// Scrollbar navbar 
+let nav = document.querySelector(".navigation-wrap");
+window.onscroll = function (){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("scroll-on");
+    }else{
+        nav.classList.remove("scroll-on");
+    }
+}
+
+// nav hide  
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
+
+ // Scroll Back To Top 
+ function scrollTopBack(){
+    let scrollTopButton = document.querySelector("#scrollUp");
+    window.onscroll = function (){
+        var scroll = document.documentElement.scrollTop;
+        if(scroll >= 250){
+            scrollTopButton.classList.add('scrollActive');
+        } else{
+            scrollTopButton.classList.remove('scrollActive');
+        }
+    }
+}
+scrollTopBack();
